c927ab0ff09b1c1c55b87d5554546c3bd4a2eff1
Luka Licina <apocpublic@outlook.com>
2026-07-31 09:19:15 +0200

    fixes for Romanian

--- cyrillic.js
+++ cyrillic.js
@@ -81,7 +81,7 @@ export class UniversalTransliterator {
             },
             romance_cyrillic: {
                 'a': 'а', 'ă': 'е', 'â': 'и', 'b': 'б', 'c': 'к', 'd': 'д', 
-                'e': 'е', 'f': 'ф', 'g': 'ґ', 'h': 'х', 'i': 'і', 'î': 'ін', 
+                'e': 'е', 'f': 'ф', 'g': 'г', 'h': 'х', 'i': 'і', 'î': 'і', 
                 'j': 'ж', 'k': 'к', 'l': 'л', 'm': 'м', 'n': 'н', 'o': 'о', 
                 'p': 'п', 'r': 'р', 's': 'с', 'ș': 'ш', 'ş': 'ш', 't': 'т', 
                 'ț': 'ц', 'ţ': 'ц', 'u': 'у', 'v': 'в', 'x': 'кс', 'z': 'з'
@@ -113,105 +113,105 @@ export class UniversalTransliterator {
             }
         };
     }
-
+    
     _preprocessDigraphs(text, languageGroup) {
         if (languageGroup === 'balkan') {
             if (/[а-яА-ЯёЁ]/.test(text)) {
                 text = this._cyrillicToLatin(text);
             }
             return text
-                .replace(/ije/g, 'іє').replace(/Ije/g, 'Іє').replace(/IJE/g, 'ІЄ')
-                .replace(/je/g, 'є').replace(/Je/g, 'Є').replace(/JE/g, 'Є')
-                .replace(/ija/g, 'ія').replace(/Ija/g, 'Ія').replace(/IJA/g, 'ІЯ')
-                .replace(/iju/g, 'ію').replace(/Iju/g, 'Ію').replace(/IJU/g, 'ІЮ')
-                .replace(/ja/g, 'я').replace(/Ja/g, 'Я').replace(/JA/g, 'Я')
-                .replace(/ju/g, 'ю').replace(/Ju/g, 'Ю').replace(/JU/g, 'Ю')
-                .replace(/lj/g, 'ль').replace(/Lj/g, 'Ль').replace(/LJ/g, 'ЛЬ')
-                .replace(/nj/g, 'нь').replace(/Nj/g, 'Нь').replace(/NJ/g, 'НЬ')
-                .replace(/dž/g, 'дж').replace(/Dž/g, 'Дж').replace(/DŽ/g, 'ДЖ')
-                .replace(/đ/g, 'джь').replace(/Đ/g, 'Джь')
-                .replace(/i/g, 'і').replace(/I/g, 'І');
+            .replace(/ije/g, 'іє').replace(/Ije/g, 'Іє').replace(/IJE/g, 'ІЄ')
+            .replace(/je/g, 'є').replace(/Je/g, 'Є').replace(/JE/g, 'Є')
+            .replace(/ija/g, 'ія').replace(/Ija/g, 'Ія').replace(/IJA/g, 'ІЯ')
+            .replace(/iju/g, 'ію').replace(/Iju/g, 'Ію').replace(/IJU/g, 'ІЮ')
+            .replace(/ja/g, 'я').replace(/Ja/g, 'Я').replace(/JA/g, 'Я')
+            .replace(/ju/g, 'ю').replace(/Ju/g, 'Ю').replace(/JU/g, 'Ю')
+            .replace(/lj/g, 'ль').replace(/Lj/g, 'Ль').replace(/LJ/g, 'ЛЬ')
+            .replace(/nj/g, 'нь').replace(/Nj/g, 'Нь').replace(/NJ/g, 'НЬ')
+            .replace(/dž/g, 'дж').replace(/Dž/g, 'Дж').replace(/DŽ/g, 'ДЖ')
+            .replace(/đ/g, 'джь').replace(/Đ/g, 'Джь')
+            .replace(/i/g, 'і').replace(/I/g, 'І');
         }
         
         if (languageGroup === 'polish_sorbian') {
             return text
-                .replace(/szcz/g, 'щ').replace(/Szcz/g, 'Щ').replace(/SZCZ/g, 'Щ')
-                .replace(/ch/g, 'х').replace(/Ch/g, 'Х').replace(/CH/g, 'Х')
-                .replace(/cz/g, 'ч').replace(/Cz/g, 'Ч').replace(/CZ/g, 'Ч')
-                .replace(/sz/g, 'ш').replace(/Sz/g, 'Ш').replace(/SZ/g, 'Ш')
-                .replace(/rz/g, 'ж').replace(/Rz/g, 'Ж').replace(/RZ/g, 'Ж')
-                .replace(/ż/g, 'ж').replace(/Ż/g, 'Ж')
-                .replace(/ł/g, 'в').replace(/Ł/g, 'В')
-                .replace(/ę/g, 'ен').replace(/Ę/g, 'Ен')
-                .replace(/ą/g, 'он').replace(/Ą/g, 'Он')
-                .replace(/ci/g, 'ці').replace(/Ci/g, 'Ці')
-                .replace(/ć/g, 'ць').replace(/Ć/g, 'Ць')
-                .replace(/si/g, 'сьі').replace(/Si/g, 'Сьі')
-                .replace(/ś/g, 'сь').replace(/Ś/g, 'Сь')
-                .replace(/zi/g, 'зьі').replace(/Zi/g, 'Зьі')
-                .replace(/ź/g, 'зь').replace(/Ź/g, 'Зь')
-                .replace(/ni/g, 'ньі').replace(/Ni/g, 'Ньі')
-                .replace(/ń/g, 'нь').replace(/Ń/g, 'Нь')
-                .replace(/w/g, 'в').replace(/W/g, 'В')
-                .replace(/i/g, 'і').replace(/I/g, 'І')
-                .replace(/y/g, 'и').replace(/Y/g, 'И');
+            .replace(/szcz/g, 'щ').replace(/Szcz/g, 'Щ').replace(/SZCZ/g, 'Щ')
+            .replace(/ch/g, 'х').replace(/Ch/g, 'Х').replace(/CH/g, 'Х')
+            .replace(/cz/g, 'ч').replace(/Cz/g, 'Ч').replace(/CZ/g, 'Ч')
+            .replace(/sz/g, 'ш').replace(/Sz/g, 'Ш').replace(/SZ/g, 'Ш')
+            .replace(/rz/g, 'ж').replace(/Rz/g, 'Ж').replace(/RZ/g, 'Ж')
+            .replace(/ż/g, 'ж').replace(/Ż/g, 'Ж')
+            .replace(/ł/g, 'в').replace(/Ł/g, 'В')
+            .replace(/ę/g, 'ен').replace(/Ę/g, 'Ен')
+            .replace(/ą/g, 'он').replace(/Ą/g, 'Он')
+            .replace(/ci/g, 'ці').replace(/Ci/g, 'Ці')
+            .replace(/ć/g, 'ць').replace(/Ć/g, 'Ць')
+            .replace(/si/g, 'сьі').replace(/Si/g, 'Сьі')
+            .replace(/ś/g, 'сь').replace(/Ś/g, 'Сь')
+            .replace(/zi/g, 'зьі').replace(/Zi/g, 'Зьі')
+            .replace(/ź/g, 'зь').replace(/Ź/g, 'Зь')
+            .replace(/ni/g, 'ньі').replace(/Ni/g, 'Ньі')
+            .replace(/ń/g, 'нь').replace(/Ń/g, 'Нь')
+            .replace(/w/g, 'в').replace(/W/g, 'В')
+            .replace(/i/g, 'і').replace(/I/g, 'І')
+            .replace(/y/g, 'и').replace(/Y/g, 'И');
         }
-
+        
         if (languageGroup === 'czech_slovak') {
             return text
-                .replace(/x/g, 'кс').replace(/X/g, 'Кс')
-                .replace(/ch/g, 'х').replace(/Ch/g, 'Х').replace(/CH/g, 'Х')
-                .replace(/ř/g, 'рж').replace(/Ř/g, 'Рж')
-                .replace(/ě/g, 'є').replace(/Ě/g, 'Є')
-                .replace(/ä/g, 'е').replace(/Ä/g, 'Е')
-                .replace(/ô/g, 'уо').replace(/Ô/g, 'Уо')
-                .replace(/ľа/g, 'ля').replace(/Ľа/g, 'Ля')
-                .replace(/ľе/g, 'лє').replace(/Ľе/g, 'Лє')
-                .replace(/іа/g, 'іа').replace(/іе/g, 'іє')
-                .replace(/y/g, 'и').replace(/Ý/g, 'И').replace(/ý/g, 'и')
-                .replace(/i/g, 'і').replace(/Í/g, 'І').replace(/í/g, 'і')
-                .replace(/ci/g, 'ці').replace(/Ci/g, 'Ці');
+            .replace(/x/g, 'кс').replace(/X/g, 'Кс')
+            .replace(/ch/g, 'х').replace(/Ch/g, 'Х').replace(/CH/g, 'Х')
+            .replace(/ř/g, 'рж').replace(/Ř/g, 'Рж')
+            .replace(/ě/g, 'є').replace(/Ě/g, 'Є')
+            .replace(/ä/g, 'е').replace(/Ä/g, 'Е')
+            .replace(/ô/g, 'уо').replace(/Ô/g, 'Уо')
+            .replace(/ľа/g, 'ля').replace(/Ľа/g, 'Ля')
+            .replace(/ľе/g, 'лє').replace(/Ľе/g, 'Лє')
+            .replace(/іа/g, 'іа').replace(/іе/g, 'іє')
+            .replace(/y/g, 'и').replace(/Ý/g, 'И').replace(/ý/g, 'и')
+            .replace(/i/g, 'і').replace(/Í/g, 'І').replace(/í/g, 'і')
+            .replace(/ci/g, 'ці').replace(/Ci/g, 'Ці');
         }
-
+        
         if (languageGroup === 'greek') {
             let cleanText = text
-                .replace(/[ά]/g, 'α').replace(/[έ]/g, 'ε')
-                .replace(/[ή]/g, 'η').replace(/[ίϊΐ]/g, 'ι')
-                .replace(/[ό]/g, 'ο').replace(/[ύϋΰ]/g, 'υ')
-                .replace(/[ώ]/g, 'ω')
-                .replace(/[Ά]/g, 'Α').replace(/[Έ]/g, 'Ε')
-                .replace(/[Ή]/g, 'Η').replace(/[Ί]/g, 'Ι')
-                .replace(/[Ό]/g, 'Ο').replace(/[Ύ]/g, 'Υ')
-                .replace(/[Ώ]/g, 'Ω');
-
+            .replace(/[ά]/g, 'α').replace(/[έ]/g, 'ε')
+            .replace(/[ή]/g, 'η').replace(/[ίϊΐ]/g, 'ι')
+            .replace(/[ό]/g, 'ο').replace(/[ύϋΰ]/g, 'υ')
+            .replace(/[ώ]/g, 'ω')
+            .replace(/[Ά]/g, 'Α').replace(/[Έ]/g, 'Ε')
+            .replace(/[Ή]/g, 'Η').replace(/[Ί]/g, 'Ι')
+            .replace(/[Ό]/g, 'Ο').replace(/[Ύ]/g, 'Υ')
+            .replace(/[Ώ]/g, 'Ω');
+            
             return cleanText
-                .replace(/ου/g, 'у').replace(/Ου/g, 'У')
-                .replace(/οι/g, 'і').replace(/Οι/g, 'І')
-                .replace(/ει/g, 'і').replace(/Ει/g, 'І')
-                .replace(/αι/g, 'е').replace(/Αι/g, 'Е')
-                .replace(/μπ/g, 'б').replace(/Μπ/g, 'Б');
+            .replace(/ου/g, 'у').replace(/Ου/g, 'У')
+            .replace(/οι/g, 'і').replace(/Οι/g, 'І')
+            .replace(/ει/g, 'і').replace(/Ει/g, 'І')
+            .replace(/αι/g, 'е').replace(/Αι/g, 'Е')
+            .replace(/μπ/g, 'б').replace(/Μπ/g, 'Б');
         }
-
+        
         if (languageGroup === 'caucasian') {
             return text
-                .replace(/(\p{L})1/gu, '$1ъ').replace(/1(\p{L})/gu, 'ъ$1')
-                .replace(/ӕ/g, 'е').replace(/Ӕ/g, 'Е')
-                .replace(/и/g, 'і').replace(/И/g, 'І')
-                .replace(/ы/g, 'и').replace(/Ы/g, 'И');
+            .replace(/(\p{L})1/gu, '$1ъ').replace(/1(\p{L})/gu, 'ъ$1')
+            .replace(/ӕ/g, 'е').replace(/Ӕ/g, 'Е')
+            .replace(/и/g, 'і').replace(/И/g, 'І')
+            .replace(/ы/g, 'и').replace(/Ы/g, 'И');
         }
-
+        
         if (languageGroup === 'eastern') {
             return text
-                .replace(/ъ/g, 'а').replace(/Ъ/g, 'А')
-                .replace(/ы/g, 'и').replace(/Ы/g, 'И')
-                .replace(/и/g, 'і').replace(/И/g, 'І');
+            .replace(/ъ/g, 'а').replace(/Ъ/g, 'А')
+            .replace(/ы/g, 'и').replace(/Ы/g, 'И')
+            .replace(/и/g, 'і').replace(/И/g, 'І');
         }
-
+        
         if (languageGroup === 'rusyn') {
             text = text.replace(/([бвгґджзклмнпрстфхцчшщ])ё/g, '$1ьо');
             text = text.replace(/([БВГҐДЖЗКЛМНПРСТФХЦЧШЩ])Ё/g, '$1ЬО');
             text = text.replace(/([БВГҐДЖЗКЛМНПРСТФХЦЧШЩ])ё/g, '$1ьо');
-
+            
             text = text.replace(/([А-ЯҐЄІЇЫЪЬ])Ё/g, '$1ЙО');
             text = text.replace(/Ё([А-ЯҐЄІЇЫЪЬ])/g, 'ЙО$1');
             text = text.replace(/Ё/g, 'Йо');
@@ -219,31 +219,35 @@ export class UniversalTransliterator {
             
             return text;
         }
-
+        
         if (languageGroup === 'romance_cyrillic') {
             return text
-                .replace(/ea\b/gi, 'я')
-                .replace(/ia\b/gi, 'я')
-                .replace(/ie\b/gi, 'є')
-                .replace(/ii\b/gi, 'ій')
-                .replace(/ia/gi, 'ія')
-                
-                .replace(/ge/gi, 'же').replace(/gi/gi, 'жі')
-                .replace(/gea/gi, 'жа').replace(/gia/gi, 'жа')
-                .replace(/ce/gi, 'че').replace(/ci/gi, 'чі')
-                .replace(/cea/gi, 'ча').replace(/cia/gi, 'ча')
-                
-                .replace(/che/gi, 'ке').replace(/chi/gi, 'кі')
-                .replace(/ghe/gi, 'ґе').replace(/ghi/gi, 'ґі')
-                .replace(/c([aouAOU])/g, 'к$1')
+            .replace(/che/gi, 'ке').replace(/chi/gi, 'кі')
+            .replace(/ghe/gi, 'ґе').replace(/ghi/gi, 'ґі')
             
-                .replace(/î/g, 'ін').replace(/î([a-z])/gi, 'і$1') // handles 'în' vs internal î
-                .replace(/â/g, 'и');
+            .replace(/cea/gi, 'ча').replace(/cia/gi, 'ча')
+            .replace(/ce/gi, 'че').replace(/ci/gi, 'чі')
+            .replace(/gea/gi, 'жа').replace(/gia/gi, 'жа')
+            .replace(/ge/gi, 'же').replace(/gi/gi, 'жі')
+            
+            .replace(/c([aouAOU])/g, 'к$1').replace(/C([aouAOU])/g, 'К$1')
+            
+            .replace(/ând/gi, 'анд').replace(/ânt/gi, 'ант')
+            
+            .replace(/eiei\b/gi, 'єй')
+            .replace(/iei\b/gi, 'ієй')
+            .replace(/ei\b/gi, 'ей')
+            .replace(/ea/gi, 'я')   
+            .replace(/ia\b/gi, 'ія')
+            .replace(/ii\b/gi, 'ій')
+            
+            .replace(/î/gi, 'і')
+            .replace(/â/gi, 'и');
         }
         
         return text;
     }
-
+    
     _cyrillicToLatin(text) {
         const cyrToLatMap = {
             'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'ђе': 'đe', 'ђ': 'đ', 'е': 'e', 'ж': 'ž',
@@ -259,28 +263,28 @@ export class UniversalTransliterator {
             return char;
         }).join('');
     }
-
+    
     transliterate(text, languageGroup) {
         const currentMap = this.maps[languageGroup];
         if (!currentMap) {
             throw new Error(`Language group '${languageGroup}' not found.`);
         }
-
+        
         let processedText = this._preprocessDigraphs(text, languageGroup);
-
+        
         return processedText.split('').map(char => {
             const lowerChar = char.toLowerCase();
-
+            
             if (currentMap[lowerChar] === undefined) {
                 return char;
             }
-
+            
             const replacement = currentMap[lowerChar];
-
+            
             if (char === char.toUpperCase() && char !== char.toLowerCase()) {
                 return replacement.charAt(0).toUpperCase() + replacement.slice(1);
             }
-
+            
             return replacement;
         }).join('');
     }
